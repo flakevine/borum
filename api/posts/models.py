a5b32b4c695953347cb2beb7ce5ef1517f11bdf3
vitormiura <vitormiura3@gmail.com>
2022-09-08 16:57:59 -0300

    feat: Init new app and add new model

---
+++ api/posts/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from users import models as user
+
+class Posts(models.Model):
+    title = models.CharField(max_length=69, null=False)
+    body = models.TextField(null=False)
+    user_id = models.ForeignKey(user.User, on_delete=models.PROTECT)
