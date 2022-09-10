3781709effbc8b279e8d9753bbef0f367d030c97
vitormiura <vitormiura3@gmail.com>
2022-09-09 22:16:51 -0300

    Start build community guide api app

--- api/guide/serializers.py
+++ api/guide/serializers.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import Guides
+
+class GuidesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Guides
+        fields = ('__all__')
--- api/guide/views.py
+++ api/guide/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-from . import views
-from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Guides
-from .serializers import 
-from rest_framework import status
+from .serializers import GuidesSerializer
+# from rest_framework import status
+# from django.shortcuts import get_object_or_404
 
 
-@
+@api_view(['GET'])
+def guides(request):
+    queryset = Guides.objects.all()
+    serializer = GuidesSerializer(queryset, many = True)
+    return Response(serializer.data)
