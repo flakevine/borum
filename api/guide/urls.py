7b98792d7c6d10792f56dbb81e8f0cf60d23260a
vitormiura <vitormiura3@gmail.com>
2022-09-09 22:44:14 -0300

    Add new forum api endpoint and display in web page

--- api/guide/urls.py
+++ api/guide/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('posts/', views.guides),
+    path('posts/<int:id>', views.guide),
 ]
--- api/guide/views.py
+++ api/guide/views.py
@@ -3,11 +3,17 @@ from rest_framework.response import Response
 from .models import Guides
 from .serializers import GuidesSerializer
 # from rest_framework import status
-# from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404
 
 
 @api_view(['GET'])
 def guides(request):
     queryset = Guides.objects.all()
     serializer = GuidesSerializer(queryset, many = True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def guide(request, id):
+    guide = get_object_or_404(Guides, pk = id)
+    serializer = GuidesSerializer(guide)
     return Response(serializer.data)
