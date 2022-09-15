b647b02759e12c8e90ce7d5d05d0f6b9499fb11a
abehidek <hidek.abe@outlook.com>
2022-09-15 16:34:42 -0300

    todo: PostsComments

--- api/forum/models.py
+++ api/forum/models.py
@@ -5,3 +5,5 @@ class Posts(models.Model):
     title = models.CharField(max_length=69, null=False)
     body = models.TextField(null=False)
     user_id = models.ForeignKey(user.User, on_delete=models.PROTECT)
+
+# class PostsComments(models.Model)
