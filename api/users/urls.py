23c08ad032f0b065c04dedfb0c93a6c886e67fb4
vitormiura <vitormiura3@gmail.com>
2022-11-01 09:25:20 -0300

    👌 IMPROVE: finished drf auth

--- api/users/urls.py
+++ api/users/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('register', views.RegisterView.as_view()),
     path('login', views.LoginView.as_view()),
+    path('user', views.UserView.as_view()),
+    path('logout', views.LogoutView.as_view()),
 ]
--- api/users/views.py
+++ api/users/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.exceptions import AuthenticationFailed
 from .models import User
 from .serializers import UserSerializer
+import jwt
+import datetime
 
 class RegisterView(APIView):
     def post(self, request):
@@ -24,4 +26,45 @@ class LoginView(APIView):
         if not user.check_password(password):
             raise AuthenticationFailed('Incorrect password!')
 
-        return Response({'message': 'Success!'})
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
+
+        response =  Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'jwt': token
+        }
+
+        return response
+
+class UserView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+        
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithm=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            'message': 'Log-out Successfully!'
+        }
+        return response
