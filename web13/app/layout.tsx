885a713d79d27b342d5dce77a977d5b3339c2659
abehidek <hidek.abe@outlook.com>
2022-11-04 09:37:57 -0300

    add: login logout functionality

--- web13/app/layout.tsx
+++ web13/app/layout.tsx
@@ -1,10 +1,32 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+  const [user, setUser] = useState(undefined);
+  useEffect(() => {
+    fetch("http://localhost:8000/users/user", { credentials: "include" })
+      .then((res) => res.json())
+      .then((res) => setUser(res));
+  }, []);
+
+  const logout = async () => {
+    await fetch("http://localhost:8000/users/logout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+
+    router.refresh();
+  };
+
   return (
     <html>
       <head></head>
@@ -17,10 +39,14 @@ export default function RootLayout({
             <li>
               <Link href="/login">Login</Link>
             </li>
+            <li>
+              <button onClick={logout}>Logout</button>
+            </li>
           </ul>
+          {user ? JSON.stringify(user) : "Loading..."}
         </nav>
         <main>{children}</main>
-        <footer>Borum</footer>
+        <footer>Footer</footer>
       </body>
     </html>
   );
--- web13/app/login/page.tsx
+++ web13/app/login/page.tsx
@@ -1,9 +1,51 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 const LoginPage = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submit = async (e: any) => {
+    e.preventDefault();
+
+    await fetch("http://localhost:8000/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    router.refresh();
+  };
+
   return (
     <div>
       <h1>Login Page</h1>
+      <form className="flex flex-col gap-2" onSubmit={submit}>
+        <input
+          type="text"
+          placeholder="email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="bg-blue-500 text-white p-5" type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
-2
+2;
 export default LoginPage;
